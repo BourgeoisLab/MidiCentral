fa739ad5a4a6d19aa60a4f351d7dd906cea226d1
Frédéric Bourgeois <bourgeoislab@gmail.com>
2020-04-12 19:57:36 +0200

    files added

---
+++ MidiCentral.ino
@@ -0,0 +1,238 @@
+#include <MIDI.h>
+#include <Encoder.h>
+#include <TimerOne.h>
+#include <Adafruit_LEDBackpack.h>
+
+// configuration
+#define MINIMUM_BPM         30
+#define MAXIMUM_BPM         300
+
+// pin definitions
+#define PIN_BTN_START      11
+#define PIN_BTN_TAP        12
+#define PIN_ENCODER_A      21
+#define PIN_ENCODER_B      20
+#define PIN_ENCODER_BTN     6
+#define PIN_CLK_IN          2
+#define PIN_CLK_OUT         3
+#define PIN_STST_IN         4
+#define PIN_STST_OUT        5
+#define PIN_LED_STARTED     22
+#define PIN_LED_STOPPED     23
+
+// MIDI definitions
+#define CLOCKS_PER_BEAT     24
+#define CLOCK_FROM_BPM(b)   (60000000UL/(CLOCKS_PER_BEAT*b))
+#define BPM_FROM_US(b)      (60000000UL/(b))
+
+// others
+#define MINIMUM_US         BPM_FROM_US(MAXIMUM_BPM)
+#define MAXIMUM_US         BPM_FROM_US(MINIMUM_BPM)
+
+// global variables
+static Adafruit_7segment disp;
+static Encoder encoder(PIN_ENCODER_A, PIN_ENCODER_B);
+static MIDI_CREATE_INSTANCE(HardwareSerial, Serial1, midiA);
+static MIDI_CREATE_INSTANCE(HardwareSerial, Serial2, midiB);
+static volatile uint16_t bpm = 120;
+static volatile bool started = false;
+static volatile uint8_t clockCount = 0;
+
+static void sendMidi(midi::MidiType type, midi::DataByte data1 = 0, midi::DataByte data2 = 0, midi::Channel channel = 0) {
+  if (type <= midi::MidiType::PitchBend) {
+    midiA.send(type, data1, data2, channel);
+    midiB.send(type, data1, data2, channel);
+    usbMIDI.send(type, data1, data2, channel, 0);
+  }
+  else {
+    midiA.sendRealTime(type);
+    midiB.sendRealTime(type);
+    usbMIDI.sendRealTime(type);
+  }
+}
+
+void interruptSendMidiClock() {
+  if (started) {
+    if (clockCount == 0) {
+      digitalWrite(PIN_CLK_OUT, LOW);
+      digitalWrite(PIN_LED_STARTED, HIGH);
+    } else if (clockCount == CLOCKS_PER_BEAT/2) {
+      digitalWrite(PIN_CLK_OUT, HIGH);
+      digitalWrite(PIN_LED_STARTED, LOW);
+    }
+    sendMidi(midi::MidiType::Clock);
+  } else {
+    if (clockCount == 0) {
+      digitalWrite(PIN_LED_STOPPED, HIGH);
+    } else if (clockCount == CLOCKS_PER_BEAT/2) {
+      digitalWrite(PIN_LED_STOPPED, LOW);
+    }
+  }
+  clockCount = (clockCount + 1) % CLOCKS_PER_BEAT;
+}
+
+static void start() {
+  if (!started) {
+    noInterrupts();
+    started = true;
+    clockCount = 0;
+    digitalWrite(PIN_LED_STOPPED, LOW);
+    digitalWrite(PIN_STST_OUT, LOW);
+    digitalWrite(PIN_CLK_OUT, HIGH);
+    sendMidi(midi::MidiType::Start);
+    interrupts();
+  }
+}
+
+static void stop() {
+  if (started) {
+    noInterrupts();
+    started = false;
+    digitalWrite(PIN_LED_STARTED, LOW);
+    digitalWrite(PIN_STST_OUT, HIGH);
+    sendMidi(midi::MidiType::Stop);
+    interrupts();
+  }
+}
+
+static void interruptStartButton() {
+  static uint32_t lastTime = 0;
+  uint32_t now = micros();
+  if (now - lastTime < 500000UL)
+    return;
+  if (started)
+    stop();
+  else
+    start();
+  lastTime = now;
+}
+
+static void interruptStartExtern() {
+  if (digitalRead(PIN_STST_IN))
+    stop();
+  else
+    start();
+}
+
+static void interruptTapButton() {
+  static uint32_t tapInterval[2];
+  static uint32_t lastTime = 0;
+  noInterrupts();
+  uint32_t now = micros();
+  uint32_t dt = now - lastTime;
+  if (dt < MINIMUM_US || dt > MAXIMUM_US) {
+    tapInterval[0] = 0;
+    tapInterval[1] = 0;
+    lastTime = now;
+  } else {
+    tapInterval[1] = tapInterval[0];
+    tapInterval[0] = dt;
+    lastTime = now;
+    if (tapInterval[0] != 0 && tapInterval[1] != 0) {
+      uint32_t interval = (tapInterval[0] + tapInterval[1]) / 2;
+      bpm = BPM_FROM_US(interval);
+    }
+  }
+  interrupts();
+}
+
+static inline void loopEncoder() {
+  static int32_t lastEncoderValue = 0;
+  int32_t encoderValue = encoder.read();
+  if (encoderValue - lastEncoderValue <= -4) {
+    bpm--;
+    lastEncoderValue = encoderValue;
+  } else if (encoderValue - lastEncoderValue >= 4) {
+    bpm++;
+    lastEncoderValue = encoderValue;
+  }
+}
+
+static inline void loopUpdateBpm() {
+  static int lastBpm = 0;
+  bpm = constrain(bpm, MINIMUM_BPM, MAXIMUM_BPM);
+  if (bpm != lastBpm) {
+    Timer1.setPeriod(CLOCK_FROM_BPM(bpm));
+    disp.print(bpm, DEC);
+    disp.writeDisplay();
+    lastBpm = bpm;
+  }
+}
+
+static inline bool filterMidiInput(midi::MidiType type) {
+  switch (type) {
+    case midi::MidiType::Start:
+      start();
+      return false;
+    case midi::MidiType::Stop:
+      stop();
+      return false;
+    case midi::MidiType::Clock:
+      return false;
+    default:
+      return true;
+  }
+}
+
+static inline void loopMidiForwarding() {
+  if (midiA.read()) {
+    midi::MidiType type = midiA.getType();
+    if (filterMidiInput(type))
+      sendMidi(type, midiA.getData1(), midiA.getData2(), midiA.getChannel());
+  }
+  if (midiB.read()) {
+    midi::MidiType type = midiB.getType();
+    if (filterMidiInput(type))
+      sendMidi(type, midiB.getData1(), midiB.getData2(), midiB.getChannel());
+  }
+  if (usbMIDI.read()) {
+    midi::MidiType type = (midi::MidiType)usbMIDI.getType();
+    if (filterMidiInput(type))
+      sendMidi(type, usbMIDI.getData1(), usbMIDI.getData2(), usbMIDI.getChannel());
+  }
+}
+
+void setup() {
+  // configure pins
+  pinMode(PIN_BTN_START, INPUT_PULLUP);
+  pinMode(PIN_BTN_TAP, INPUT_PULLUP);
+  pinMode(PIN_ENCODER_BTN, INPUT_PULLUP);
+  pinMode(PIN_CLK_IN, INPUT);
+  pinMode(PIN_CLK_OUT, OUTPUT);
+  pinMode(PIN_STST_IN, INPUT);
+  pinMode(PIN_STST_OUT, OUTPUT);
+  pinMode(PIN_LED_STARTED, OUTPUT);
+  pinMode(PIN_LED_STOPPED, OUTPUT);
+
+  // attach interrupts
+  attachInterrupt(digitalPinToInterrupt(PIN_BTN_START), interruptStartButton, FALLING);
+  attachInterrupt(digitalPinToInterrupt(PIN_BTN_TAP), interruptTapButton, FALLING);
+  attachInterrupt(digitalPinToInterrupt(PIN_STST_IN), interruptStartExtern, CHANGE);
+
+  // initialize display
+  disp.begin(0x70);
+
+  // initialize MIDI A
+  midiA.begin(MIDI_CHANNEL_OMNI);
+  midiA.turnThruOff();
+
+  // initialize MIDI B
+  midiB.begin(MIDI_CHANNEL_OMNI);
+  midiB.turnThruOff();
+
+  // initialize timer to send the MIDI clock
+  Timer1.initialize(CLOCK_FROM_BPM(bpm));
+  Timer1.attachInterrupt(interruptSendMidiClock);
+}
+
+void loop() {
+
+  // read encoder
+  loopEncoder();
+
+  // update bpm
+  loopUpdateBpm();
+
+  // forward MIDI messages
+  loopMidiForwarding();
+}
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# MidiCentral
-Microcontroller based device to centralize MIDI messages
+# MIDI Central
+Microcontroller based device to centralize MIDI messages.
+
+<img src="images/01.jpg" width="240"><img src="images/02.jpg" width="240"><img src="images/03.jpg" width="240"><img src="images/04.jpg" width="240"><img src="images/05.jpg" width="240">
+
+## Features
+### Software
+- Arduino based code
+- Merges up to 3 different MIDI inputs
+- MIDI to CV clock converter and vice versa
+- Act as a MIDI device when plugged in a computer
+
+### Hardware
+- 2x MIDI inputs
+- 8x MIDI outputs
+- 1x CV clock input and output
+- 1x CV start/stop input and output
+- 1x start/stop button
+- 1x tap button
+- 1x encoder
+- 1x 4-Digit 7-Segment Display
+- Teensy 3.2 (32 bit ARM Cortex-M4 72 MHz CPU)
+- USB powered
