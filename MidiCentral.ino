5bbce0754e5a5792d178018bec541f94fd488313
Frédéric Bourgeois <bourgeoislab@gmail.com>
2020-12-25 15:11:53 +0100

    system reset, stop/continue

--- MidiCentral.ino
+++ MidiCentral.ino
@@ -3,22 +3,25 @@
 #include <TimerOne.h>
 #include <Adafruit_LEDBackpack.h>
 
-// configuration
-#define MINIMUM_BPM         30
-#define MAXIMUM_BPM         300
+// TODO:
+// option for active-sensing (send at <300ms)
+// select clock source
 
 // pin definitions
-#define PIN_BTN_START      11
-#define PIN_BTN_TAP        12
-#define PIN_ENCODER_A      21
-#define PIN_ENCODER_B      20
+#define PIN_BTN_START       11
+#define PIN_BTN_TAP         12
+#define PIN_ENCODER_A       21
+#define PIN_ENCODER_B       20
 #define PIN_ENCODER_BTN     6
-#define PIN_CLK_IN          2
+#define PIN_CLK_IN          2   // not used yet
 #define PIN_CLK_OUT         3
 #define PIN_STST_IN         4
 #define PIN_STST_OUT        5
 #define PIN_LED_STARTED     22
 #define PIN_LED_STOPPED     23
+
+// I2C address of 7-segment display
+#define DISP_I2C_ADDR       0x70
 
 // MIDI definitions
 #define CLOCKS_PER_BEAT     24
@@ -26,8 +29,15 @@
 #define BPM_FROM_US(b)      (60000000UL/(b))
 
 // others
-#define MINIMUM_US         BPM_FROM_US(MAXIMUM_BPM)
-#define MAXIMUM_US         BPM_FROM_US(MINIMUM_BPM)
+#define MINIMUM_US          BPM_FROM_US(MAXIMUM_BPM)
+#define MAXIMUM_US          BPM_FROM_US(MINIMUM_BPM)
+
+// minimal/maximal BPM
+#define MINIMUM_BPM         30
+#define MAXIMUM_BPM         300
+
+// debounce time for start/stop button
+#define BTN_START_DEBOUNCE  200
 
 // global variables
 static Adafruit_7segment disp;
@@ -37,6 +47,7 @@ static MIDI_CREATE_INSTANCE(HardwareSerial, Serial2, midiB);
 static volatile uint16_t bpm = 120;
 static volatile bool started = false;
 static volatile uint8_t clockCount = 0;
+static volatile uint32_t btnStartPressedTime = 0;
 
 static void sendMidi(midi::MidiType type, midi::DataByte data1 = 0, midi::DataByte data2 = 0, midi::Channel channel = 0) {
   if (type <= midi::MidiType::PitchBend) {
@@ -71,7 +82,7 @@ void interruptSendMidiClock() {
   clockCount = (clockCount + 1) % CLOCKS_PER_BEAT;
 }
 
-static void start() {
+static void start(midi::MidiType type) {
   if (!started) {
     noInterrupts();
     started = true;
@@ -79,7 +90,7 @@ static void start() {
     digitalWrite(PIN_LED_STOPPED, LOW);
     digitalWrite(PIN_STST_OUT, LOW);
     digitalWrite(PIN_CLK_OUT, HIGH);
-    sendMidi(midi::MidiType::Start);
+    sendMidi(type);
     interrupts();
   }
 }
@@ -90,28 +101,48 @@ static void stop() {
     started = false;
     digitalWrite(PIN_LED_STARTED, LOW);
     digitalWrite(PIN_STST_OUT, HIGH);
+    digitalWrite(PIN_CLK_OUT, HIGH);
     sendMidi(midi::MidiType::Stop);
     interrupts();
   }
 }
 
 static void interruptStartButton() {
-  static uint32_t lastTime = 0;
-  uint32_t now = micros();
-  if (now - lastTime < 500000UL)
-    return;
-  if (started)
+  if (!digitalRead(PIN_BTN_START)) {
+    static uint32_t lastTime = 0;
+    uint32_t now = millis();
+    if (now - lastTime < BTN_START_DEBOUNCE)
+      return;
+    if (started)
+      stop();
+    else
+      start(midi::MidiType::Start);
+    lastTime = now;
+    btnStartPressedTime = now;
+  } else{
+    btnStartPressedTime = 0;
+  }
+}
+
+static void checkSystemReset() {
+  uint32_t now = millis();
+  if (btnStartPressedTime > 0 && now - btnStartPressedTime > 2000) {
+    btnStartPressedTime = 0;
+    disp.printError();
+    disp.writeDisplay();
     stop();
-  else
-    start();
-  lastTime = now;
+    sendMidi(midi::MidiType::SystemReset);
+    delay(500);
+    disp.print(bpm, DEC);
+    disp.writeDisplay();
+  }
 }
 
 static void interruptStartExtern() {
   if (digitalRead(PIN_STST_IN))
     stop();
   else
-    start();
+    start(midi::MidiType::Start);
 }
 
 static void interruptTapButton() {
@@ -136,7 +167,19 @@ static void interruptTapButton() {
   interrupts();
 }
 
-static inline void loopEncoder() {
+static void interruptEncoderButton() {
+  static uint32_t lastTime = 0;
+  uint32_t now = millis();
+  if (now - lastTime < BTN_START_DEBOUNCE)
+    return;
+  if (started)
+    stop();
+  else
+    start(midi::MidiType::Continue);
+  lastTime = now;
+}
+
+static inline void readEncoder() {
   static int32_t lastEncoderValue = 0;
   int32_t encoderValue = encoder.read();
   if (encoderValue - lastEncoderValue <= -4) {
@@ -148,7 +191,7 @@ static inline void loopEncoder() {
   }
 }
 
-static inline void loopUpdateBpm() {
+static inline void updateBpm() {
   static int lastBpm = 0;
   bpm = constrain(bpm, MINIMUM_BPM, MAXIMUM_BPM);
   if (bpm != lastBpm) {
@@ -162,7 +205,8 @@ static inline void loopUpdateBpm() {
 static inline bool filterMidiInput(midi::MidiType type) {
   switch (type) {
     case midi::MidiType::Start:
-      start();
+    case midi::MidiType::Continue:
+      start(type);
       return false;
     case midi::MidiType::Stop:
       stop();
@@ -174,7 +218,7 @@ static inline bool filterMidiInput(midi::MidiType type) {
   }
 }
 
-static inline void loopMidiForwarding() {
+static inline void forwardMidi() {
   if (midiA.read()) {
     midi::MidiType type = midiA.getType();
     if (filterMidiInput(type))
@@ -205,12 +249,13 @@ void setup() {
   pinMode(PIN_LED_STOPPED, OUTPUT);
 
   // attach interrupts
-  attachInterrupt(digitalPinToInterrupt(PIN_BTN_START), interruptStartButton, FALLING);
+  attachInterrupt(digitalPinToInterrupt(PIN_BTN_START), interruptStartButton, CHANGE);
   attachInterrupt(digitalPinToInterrupt(PIN_BTN_TAP), interruptTapButton, FALLING);
+  attachInterrupt(digitalPinToInterrupt(PIN_ENCODER_BTN), interruptEncoderButton, FALLING);
   attachInterrupt(digitalPinToInterrupt(PIN_STST_IN), interruptStartExtern, CHANGE);
 
   // initialize display
-  disp.begin(0x70);
+  disp.begin(DISP_I2C_ADDR);
 
   // initialize MIDI A
   midiA.begin(MIDI_CHANNEL_OMNI);
@@ -226,13 +271,15 @@ void setup() {
 }
 
 void loop() {
-
+  // check for system reset
+  checkSystemReset();
+    
   // read encoder
-  loopEncoder();
+  readEncoder();
 
   // update bpm
-  loopUpdateBpm();
+  updateBpm();
 
   // forward MIDI messages
-  loopMidiForwarding();
+  forwardMidi();
 }
